55f924556f9b8101a358283196ff1987ce5bda69
Semeton <88825982+Semeton@users.noreply.github.com>
2022-06-07 16:17:54 +0100

    Controllers created

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -2,6 +2,12 @@ const db = require("../models");
 const Product = db.products;
 const Op = db.Sequelize.Op;
 
+/**
+ *
+ * @param {name, description, available} req
+ * @param {new product} res
+ * @returns new products;
+ */
 exports.create = (req, res) => {
   if (!req.body.description) {
     res.status(400).send({
@@ -30,9 +36,151 @@ exports.create = (req, res) => {
       });
     });
 };
-exports.findAll = (req, res) => {};
-exports.findOne = (req, res) => {};
-exports.update = (req, res) => {};
-exports.delete = (req, res) => {};
-exports.deleteAll = (req, res) => {};
-exports.finAllAvailable = (req, res) => {};
+
+/**
+ *
+ * @param {name} req
+ * @param {similar products} res
+ * @returns similar products;
+ */
+exports.findAll = (req, res) => {
+  const name = req.query.name;
+  var condition = title ? { title: { [Op.iLike]: `%{title}` } } : null;
+
+  /**
+   * Retrieves all matching products
+   */
+  Product.findAll({ where: condition })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving products.",
+      });
+    });
+};
+
+/**
+ *
+ * @param {product id} req
+ * @param {product wuth id} res
+ * @returns product with id;
+ */
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  /**
+   * Retrieve matching product
+   */
+  Product.findByPk(id)
+    .then((data) => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Product with id=${id}.`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error retrieving Product with id=" + id,
+      });
+    });
+};
+
+exports.update = (req, res) => {
+  const id = req.params.id;
+  /**
+   * Update specific product
+   */
+  Product.update(req.body, {
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Product was deleted successfully",
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Product with id=${id}. Maybe Product was not found or req.body is empty!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error update Product with id=" + id,
+      });
+    });
+};
+
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  /**
+   * Update specific product
+   */
+  Product.destroy({
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Product was deleted successfully",
+        });
+      } else {
+        res.send({
+          message: `Cannot update Product with id=${id}. Maybe Product was not found or req.body is empty!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete Product with id=" + id,
+      });
+    });
+};
+
+/**
+ *
+ * @param {} req
+ * @param {*} res
+ * @returns delete all products;
+ */
+exports.deleteAll = (req, res) => {
+  Tutorial.destroy({
+    where: {},
+    truncate: false,
+  })
+    .then((nums) => {
+      res.send({ message: `${nums} Products were deleted successfully!` });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all tutorals.",
+      });
+    });
+};
+
+/**
+ *
+ * @param {} req
+ * @param {*} res
+ * @returns Find all available products;
+ */
+exports.finAllAvailable = (req, res) => {
+  Tutorial.findAll({ where: { available: true } })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorals.",
+      });
+    });
+};
